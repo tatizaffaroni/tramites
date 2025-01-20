0c0aab2c5999b276d21498c2158959451d288b97
tatizaffaroni <tatizaffaroni@yahoo.com.ar>
2025-01-20 19:15:15 -0300

    Update caratula.js

--- caratula.js
+++ caratula.js
@@ -1,13 +1,13 @@
 window.onload = function() {
     // Obtener el valor de los parámetroa de la URL
     const urlParams = new URLSearchParams(window.location.search);
-    const instrumeto = urlParams.get('instrumeto');
+    const instrumento = urlParams.get('instrumento');
     const otorga = urlParams.get('otorga');
     const afavorde = urlParams.get('afavorde');
     const fecha = urlParams.get('fecha');
     
     // Modifica los valores en el html según su ID
-    document.getElementById('instrumeto').innerText = instrumeto;
+    document.getElementById('instrumento').innerText = instrumento;
     document.getElementById('otorga').innerText = otorga;
     document.getElementById('afavorde').innerText = afavorde;
     document.getElementById('fecha').innerText = fecha;
